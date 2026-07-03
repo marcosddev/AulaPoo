07470adc75661bbf82683ecb569209a61e60292f
Marcos Davi <marcosdv2006@gmail.com>
2026-07-03 00:50:29 -0300

    Update AreaCirculo.java

--- src/AreaCirculo.java
+++ src/AreaCirculo.java
@@ -4,10 +4,10 @@ public class AreaCirculo {
     public void calcularAreaCirculo(Scanner entrada){
         double raio, area;
 
-        //ESCREVAL("Digite o raio do circulo: ")
+        
         System.out.println("Digite o raio do circulo: ");
         raio = entrada.nextDouble();
-        //LEIA(RAIO)
+       
 
         area = Math.PI * raio * raio;
     	System.out.printf("A área do círculo é: %.2f\n", area);
